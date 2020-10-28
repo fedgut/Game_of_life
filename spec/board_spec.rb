87a15746864aa503a2dca6a062ffe3a1c8c261c4
fedgut <fedgut@gmail.com>
2020-10-28 15:23:47 -0600

    Fix test bug

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -16,15 +16,18 @@ RSpec.describe Board do
   end
 
   context 'Should generate an array of cells from the seed' do
-
+    board.populate_grid
     it 'Gridd should be an array of the same lenght as the seed' do
       expect(board.grid).to be_an_instance_of(Array)
       expect(board.grid.length).to eq(seed.length)
       expect(board.grid[0].length).to eq(seed[0].length)
     end
 
-    it 'Should be populated by cells' do
-      expect(board.grid.each).to be_an_instance_of(Cell)
+    it 'Should be populated by arrays of cells' do
+      expect(board.grid.all? { |sub_array| sub_array.class == Array}).to be(true)
+      expect(board.grid.all? do |sub_array|
+        sub_array.all? { |cell| cell.class == Cell }
+      end).to be(true)
     end
   end
 end
