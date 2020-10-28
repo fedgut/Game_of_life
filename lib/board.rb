2a3c858fe6e5339c9a897c3f98715e6b98cd2bae
fedgut <fedgut@gmail.com>
2020-10-28 15:23:13 -0600

    Create populate_grid method

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,18 @@
+require './lib/cell'
+
 class Board
   attr_reader :grid
   def initialize(seed)
     @seed = seed
+    @grid = []
   end
 
-  def populate_grid 
+  def populate_grid
+    @seed.each do |sub_array|
+      inner_array = sub_array.map do |item|
+        Cell.new(item)
+      end
+      @grid.push(inner_array)
+    end
   end
 end
